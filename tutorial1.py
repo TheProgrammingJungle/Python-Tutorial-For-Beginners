75fb49929cf609be392c8643df61bef52e6e0263
Yash <yash@theprogrammingjungle.com>
2020-09-28 10:13:38 +0530

    Code for Tutorial-1

---
+++ tutorial1.py
@@ -0,0 +1,10 @@
+# Python Programming Tutorial for Beginners
+# Part-1: Installation, Hello World program, and Comments
+
+print("Hello world")
+print("Programming is awesome")
+
+# Comments are used to explain your code.
+# They start with a '#' symbol.
+# These are not executed by python interpreter.
+# Basically these lines are skipped.
